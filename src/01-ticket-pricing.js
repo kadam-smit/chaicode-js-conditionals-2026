6fdf0f578623abddc519200bd35bf75b448985a3
kadam-smit <smitkadam.dev@gmail.com>
2026-07-31 14:45:27 +0530

    completed first test

--- src/01-ticket-pricing.js
+++ src/01-ticket-pricing.js
@@ -23,5 +23,30 @@
  * @returns {number} The ticket price, or -1 for invalid input
  */
 export function getTicketPrice(age, isWeekend) {
-  // Your code here
+
+  if (typeof age !== "number" || age < 0) {
+    return -1;
+  }
+  let price;
+
+  if (age <= 12) {
+    price = 8;
+  } else if (age <= 17) {
+    price = 12;
+  } else if (age <= 59) {
+    price = 15;
+  } else {
+    price = 10;
+  }
+
+  if (isWeekend) {
+    price += 3
+  }
+
+  return price;
 }
+
+console.log(getTicketPrice(19, true));
+console.log(getTicketPrice(89, false));
+
+
