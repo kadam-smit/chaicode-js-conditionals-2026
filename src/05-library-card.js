101fe4ba0080a83d62f13f88afe323f2f93f79b5
kadam-smit <smitkadam.dev@gmail.com>
2026-08-02 14:06:31 +0530

    Fifth done

--- src/05-library-card.js
+++ src/05-library-card.js
@@ -33,5 +33,33 @@
  * @returns {{ allowed: boolean, message: string }}
  */
 export function canBorrowBook(memberAge, hasValidCard, overdueBooks) {
-  // Your code here
+  if(memberAge<6){
+    return {
+      allowed : false,
+      message : "Too young - must be at least 6 years old"
+    }
+  }
+  if(!hasValidCard){
+    return {
+      allowed : false,
+      message: "Invalid library card - please renew at the front desk"
+    }
+  }
+  if(overdueBooks>0){
+    return {
+      allowed : false,
+      message : `Please return your ${overdueBooks} Books`
+    }
+  }
+  else {
+    return {
+      allowed : true ,
+      message : "You may borrow up to 3 books"
+    }
+  }
 }
+
+
+console.log(canBorrowBook(12,true,0));
+console.log(canBorrowBook(21,true,2));
+console.log(canBorrowBook(3,false,0));
